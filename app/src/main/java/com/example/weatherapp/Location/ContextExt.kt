71fd74db6cdfd9ef4ca6b5bd2cac4bf473106921
Faraz Siddiqui <farazsiddiqui2003@gmail.com>
2025-07-21 14:39:34 +0500

    checking permission

--- app/src/main/java/com/example/weatherapp/Location/ContextExt.kt
+++ app/src/main/java/com/example/weatherapp/Location/ContextExt.kt
@@ -4,6 +4,7 @@ import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.core.content.ContextCompat
 
 fun Context.hasLocationPermission(): Boolean {
@@ -14,4 +15,13 @@ fun Context.hasLocationPermission(): Boolean {
                 this,
                 ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
+}
+
+fun Context.isDeviceGPSenable(): Boolean {
+
+    val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) &&
+            locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
 }
